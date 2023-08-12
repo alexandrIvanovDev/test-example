9cde631994595dd9fa9150d57457159117e7d35f
AlexandrIvanov <alexandr.1996@list.ru>
2023-08-12 16:29:02 +0300

    Change select

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,21 +1,54 @@
-import React, {FC, SelectHTMLAttributes} from 'react';
+import React, {FC, MouseEventHandler, useState} from 'react';
 import cl from './Select.module.css'
 import classNames from 'classnames';
 
-interface PropsType extends SelectHTMLAttributes<HTMLSelectElement> {
-    addClass?: string
+export type OptionType = {
+    label: string
+    value: any
 }
 
-export const Select: FC<PropsType> = (props) => {
+type PropsType =  {
+    addClass?: string
+    options: Array<OptionType>
+    value: any
+    onChange: (value: OptionType) => void
+}
 
-    const {addClass, ...otherProps} = props
+export const Select: FC<PropsType> = ({addClass, options, value, onChange}) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const [highlightedIndex, setHighlightedIndex] = useState(0)
+
+    const toggle = () => {
+        setIsOpen(prevState => !prevState)
+    }
+
+    const isOptionSelected = (option: OptionType) => {
+        return value === option
+    }
 
     return (
-        <select className={classNames(addClass, cl.select)}{...otherProps}>
-            <option value="" disabled selected>Тема сообщения</option>
-            <option value="theme1">Тема 1</option>
-            <option value="theme2">Тема 2</option>
-            <option value="theme3">Тема 3</option>
-        </select>
+        <div className={classNames(cl.container, {[cl.show]: isOpen}, [addClass])} onClick={toggle} onBlur={() => setIsOpen(false)} tabIndex={0}>
+            <span className={cl.value}>{value.label}</span>
+            <ul className={classNames(cl.options, {[cl.show]: isOpen}, [])}>
+                {options.map((option, index) => {
+
+                    const changeOption = (e: React.MouseEvent<HTMLLIElement>) => {
+                        e.stopPropagation()
+                        onChange(option)
+                        setIsOpen(false)
+                    }
+
+                     return <li
+                         key={option.value}
+                         className={classNames(cl.option, {[cl.selected]: isOptionSelected(option), [cl.highlighted]: index === highlightedIndex})}
+                         onClick={changeOption}
+                         onMouseEnter={() => setHighlightedIndex(index)}
+                     >
+                         {option.label}
+                    </li>
+                })}
+            </ul>
+        </div>
     )
 };
